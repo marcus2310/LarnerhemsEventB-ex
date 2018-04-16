9c5f18cdcd0991dbfc99619f78e1418e6de7faab
marcus2310 <lindberg__94@hotmail.com>
2018-04-16 10:27:11 +0200

    Add project files.

---
+++ LarnerhemsEvent/Controllers/BokaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LarnerhemsEvent.Controllers
+{
+    public class BokaController : Controller
+    {
+        // GET: Boka
+        public ActionResult Boka()
+        {
+
+
+            return View();
+        }
+    }
+}
