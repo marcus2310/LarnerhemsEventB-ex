bb55fa1940a0eada9919d7f105c69453b981f85a
marcus2310 <lindberg__94@hotmail.com>
2018-05-02 10:54:48 +0200

    Skapat databas och connection.

---
+++ LarnerhemsEvent/DBOperations/DbOp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using LarnerhemsEvent.Models;
+
+namespace LarnerhemsEvent.DBOperations
+{
+    public class DbOp
+    {
+        private leventsyd_se_dbEntities db = new leventsyd_se_dbEntities();
+
+
+        
+
+
+    }
+}
