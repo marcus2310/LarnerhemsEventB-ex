de38e28eadb12214ae1609930371f5687f8af78b
marcus2310 <lindberg__94@hotmail.com>
2018-05-14 15:55:29 +0200

    Skicka data från vyn till controller för att spara paket till tempdata för att sedan kunna slänga in i OrderDatabas

--- LarnerhemsEvent/Controllers/BokaController.cs
+++ LarnerhemsEvent/Controllers/BokaController.cs
@@ -18,7 +18,11 @@ namespace LarnerhemsEvent.Controllers
         List<package> soundPackList = new List<package>();
         List<package> LightPackList = new List<package>();
         List<package> ExtraPackList = new List<package>();
-        int[] ArrayofPackageID;
+
+        List<string> PackageIDList = new List<string>();
+
+
+        List<package> ChoosenPackages = new List<package>();
 
 
 
@@ -34,20 +38,57 @@ namespace LarnerhemsEvent.Controllers
 
             return View(tentpackList);
         }
+       
+       
         [HttpPost]
-        public ActionResult Index(package pg)
+      
+        public ActionResult Index(FormCollection form)
         {
-            tentpackList = dbc.GetTentPackages();
+            try
+            {
+                var PackageId = form["buttonItem"];
+                int itemID = Convert.ToInt32(PackageId);
+
+                var item = dbc.GetAPackage(itemID);
+                TempData["tentItem"] = item.packageID;
+
+            }
+            catch (Exception)
+            {
 
 
 
-            return View(tentpackList);
+            }
+            
+
+            return RedirectToAction("Golv", "Boka");
         }
-        public ActionResult Golv()
+
+            public ActionResult Golv()
         {
             floorPackList = dbc.GetFloorPackages();
 
             return View (floorPackList);
+        }
+        [HttpPost]
+        public ActionResult Golv(FormCollection form)
+        {
+            try
+            {
+                var PackageId = form["buttonItem"];
+                int itemID = Convert.ToInt32(PackageId);
+
+                var item = dbc.GetAPackage(itemID);
+
+                TempData["floorItem"] = item.packageID;
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return RedirectToAction("Ljud", "Boka");
         }
 
         public ActionResult Ljud()
@@ -56,6 +97,26 @@ namespace LarnerhemsEvent.Controllers
 
             return View(soundPackList);
         }
+        [HttpPost]
+        public ActionResult Ljud(FormCollection form)
+        {
+            try
+            {
+                var PackageId = form["buttonItem"];
+                int itemID = Convert.ToInt32(PackageId);
+
+                var item = dbc.GetAPackage(itemID);
+
+                TempData["soundItem"] = item.packageID;
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return RedirectToAction("Ljus", "Boka");
+        }
 
         public ActionResult Ljus()
         {
@@ -63,16 +124,87 @@ namespace LarnerhemsEvent.Controllers
 
             return View(LightPackList);
         }
+        [HttpPost]
+        public ActionResult Ljus(FormCollection form)
+        {
+            try
+            {
+                var PackageId = form["buttonItem"];
+                int itemID = Convert.ToInt32(PackageId);
 
+                var item = dbc.GetAPackage(itemID);
+
+                TempData["lightItem"] = item.packageID;
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return RedirectToAction("Tillbehor", "Boka");
+        }
         public ActionResult Tillbehor()
         {
             ExtraPackList = dbc.GetExtraPackages();
 
             return View(ExtraPackList);
         }
+        [HttpPost]
+        public ActionResult Tillbehor(FormCollection form)
+        {
+            try
+            {
+                var PackageId1 = form["13"];
+                var PackageId2 = form["14"];
+                var PackageId3 = form["15"];
+                var PackageId4 = form["16"];
+                var PackageId5 = form["17"];
+                var PackageId6 = form["18"];
+
+                int itemID1 = Convert.ToInt32(PackageId1);
+                int itemID2 = Convert.ToInt32(PackageId2);
+                int itemID3 = Convert.ToInt32(PackageId3);
+                int itemID4 = Convert.ToInt32(PackageId4);
+                int itemID5 = Convert.ToInt32(PackageId5);
+                int itemID6 = Convert.ToInt32(PackageId6);
+
+                var item1 = dbc.GetAPackage(13);
+                var item2 = dbc.GetAPackage(14);
+                var item3 = dbc.GetAPackage(15);
+                var item4 = dbc.GetAPackage(16);
+                var item5 = dbc.GetAPackage(17);
+                var item6 = dbc.GetAPackage(18);
+
+
+
+                TempData["TillbehorItem1"] = item1.packageID;
+                TempData["TillbehorItem2"] = item2.packageID;
+                TempData["TillbehorItem3"] = item3.packageID;
+                TempData["TillbehorItem4"] = item4.packageID;
+                TempData["TillbehorItem5"] = item5.packageID;
+                TempData["TillbehorItem6"] = item6.packageID;
+            }
+            catch (Exception)
+            {
+
+               
+            }
+
+            return RedirectToAction("Slutfor", "Boka");
+        }
         public ActionResult Slutfor()
         {
+            foreach (var i in TempData)
+            {
+                var tempItem = Convert.ToString(i.Value);
+                PackageIDList.Add(tempItem);
+
+            }
+
             allPackList = dbc.GetAllPackages();
+
+            
 
             return View(allPackList);
         }
--- LarnerhemsEvent/DBOperations/DbOp.cs
+++ LarnerhemsEvent/DBOperations/DbOp.cs
@@ -13,40 +13,61 @@ namespace LarnerhemsEvent.DBOperations
 
 
 
-      
+        //hämtar ett specifikt paket med ID
+        public package GetAPackage(int id)
+        {
+
+            var pack = db.packages.Find(id);
+
+
+            return pack;
+        }
+        //hämta en specifik order och skicka tillbaka paket som ligger i den ordern.
+        public List<package> GetOrderPackages(int id)
+        {
+
+            var order = db.orders.Find(id);
+
+            var OrderPackages = order.packages.ToList();
+
+            return OrderPackages;
+        }
+        //hämtar alla tältpaket
         public List<package> GetTentPackages()
         {
             var tentpackages = db.packages.Where(x => x.fk_genre_id == 1).ToList();
 
             return tentpackages;
         }
+        //hämtar alla golvpaket
         public List<package> GetFloorPackages()
         {
             var floorPackages = db.packages.Where(x => x.fk_genre_id == 2).ToList();
 
             return floorPackages;
         }
-
+        //hämtar alla ljudpaket
         public List<package> GetSoundPackages()
         {
             var SoundPackages = db.packages.Where(x => x.fk_genre_id == 3).ToList();
 
             return SoundPackages;
         }
+        //hämtar alla ljuspaket
         public List<package> GetLightPackages()
         {
             var LightPackages = db.packages.Where(x => x.fk_genre_id == 4).ToList();
 
             return LightPackages;
         }
-
+        //hämtar alla tillbehör
         public List<package> GetExtraPackages()
         {
             var ExtraPackages = db.packages.Where(x => x.fk_genre_id == 5).ToList();
 
             return ExtraPackages;
         }
-
+        //hämtar alla paket som finns
         public List<package>GetAllPackages()
         {
             var packages = db.packages.ToList(); 
@@ -54,11 +75,5 @@ namespace LarnerhemsEvent.DBOperations
             return packages; 
         }
 
-
-
-        //public List<prodpackdetails> GetAllPackProd()
-        //{
-        //    var packprod = db.packages.Where( )
-        //}
     }
 }
